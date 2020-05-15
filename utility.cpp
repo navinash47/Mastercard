11b8d1a25ca59014b1e6e22b3976b1234fa2a484
Avinash Nandyala <navinash47@gmail.com>
2020-05-15 16:54:43 +0530

    changed whole thing

--- utility.cpp
+++ utility.cpp
@@ -1,37 +1,172 @@
-#include "utility.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream> // std::stringstream
+using namespace std;
+typedef struct _tlv
+{
+
+    int length;
+    string tag;
+    string value;
+    struct _tlv *child;
+    struct _tlv *next;
+} TLV;
+
+//functions
+TLV *get_node_tlv_quad(int curr, string cmd, int length, string tag_four);
+TLV *get_node_tlv_double(int curr, string cmd, int length, string tag_two);
+TLV *parse_tlv(string cmd);
+int Hextodec(string hexVal);
+void print_node(TLV *node, int level);
+bool search(string tag);
+
+string rapdu[] = {"42", "4F", "57", "50", "5A", "5F20", "5F24", "5F25", "5F28", "5F2A", "5F2D", "5F30", "5F34", "5F36", "5F50", "5F54", "5F55", "5F53",
+                  "5F56",
+                  "5F57",
+                  "61",
+                  "6F",
+                  "70",
+                  "71",
+                  "72",
+                  "73",
+                  "77",
+                  "80",
+                  "81",
+                  "82",
+                  "83",
+                  "84",
+                  "86",
+                  "87",
+                  "88",
+                  "89",
+                  "8A",
+                  "8C",
+                  "8D",
+                  "8E",
+                  "8F",
+                  "90",
+                  "91",
+                  "92",
+                  "93",
+                  "94",
+                  "95",
+                  "97",
+                  "98",
+                  "99",
+                  "9A",
+                  "9B",
+                  "9C",
+                  "9D",
+                  "9F01",
+                  "9F02",
+                  "9F03",
+                  "9F04",
+                  "9F05",
+                  "9F06",
+                  "9F07",
+                  "9F08",
+                  "9F09",
+                  "9F0B",
+                  "9F0D",
+                  "9F0E",
+                  "9F0F",
+                  "9F10",
+                  "9F11",
+                  "9F12",
+                  "9F13",
+                  "9F14",
+                  "9F15",
+                  "9F16",
+                  "9F17",
+                  "9F18",
+                  "9F1A",
+                  "9F1B",
+                  "9F1C",
+                  "9F1D",
+                  "9F1E",
+                  "9F1F",
+                  "9F20",
+                  "9F21",
+                  "9F22",
+                  "9F23",
+                  "9F26",
+                  "9F27",
+                  "9F2D",
+                  "9F2E",
+                  "9F2F",
+                  "9F32",
+                  "9F33",
+                  "9F34",
+                  "9F35",
+                  "9F36",
+                  "9F37",
+                  "9F38",
+                  "9F39",
+                  "9F3A",
+                  "9F3B",
+                  "9F3C",
+                  "9F3D",
+                  "9F40",
+                  "9F41",
+                  "9F42",
+                  "9F43",
+                  "9F44",
+                  "9F45",
+                  "9F46",
+                  "9F47",
+                  "9F48",
+                  "9F49",
+                  "9F4A",
+                  "9F4B",
+                  "9F4C",
+                  "9F4D",
+                  "9F4E",
+                  "9F4F",
+                  "A5",
+                  "BF0C"};
 using namespace std;
 
-TLV *parse_tlv(char *cmd)
+TLV *parse_tlv(string cmd)
 {
     int curr = 0;
     TLV *head = NULL;
-    // if (cmd[curr] == 8)
-    // cout << cmd << endl;
-    // cout << strlen(cmd) << ">" << curr << endl;
-    while (strlen(cmd) > curr)
+    while (cmd.length() > curr)
     {
-        // cout << "curr:" << curr << endl;
-        // cout << strlen(cmd) << ">" << curr << endl;
-        char *tag_four = new char[5];
-        char *tag_two = new char[5];
+        string tag_four, tag_two, len;
+        int Lval;
 
-        tag_four[0] = cmd[curr];
-        tag_four[1] = cmd[curr + 1];
-        tag_four[2] = cmd[curr + 2];
-        tag_four[3] = cmd[curr + 3];
-        tag_four[4] = '\0';
+        tag_four = cmd.substr(curr, 4);
+        tag_two = cmd.substr(curr, 2);
 
-        tag_two[0] = cmd[curr];
-        tag_two[1] = cmd[curr + 1];
-        tag_two[2] = '\0';
-
-        if (search("tlv_csv.csv", tag_four))
+        if (search(tag_four))
         {
-            cout << "tagfour" << tag_four << endl;
-            curr += 6;
-            TLV *node = get_node_tlv_quad(curr, cmd);
+
+            // cout << "tagfour" << tag_four << endl;
+            curr += 4;
+            if (cmd[curr] == '8' && cmd[curr + 1] == '1')
+            {
+                curr += 2;
+                len = cmd.substr(curr, 2);
+                Lval = Hextodec(len);
+                curr += 2;
+            }
+            else if (cmd[curr] == '8' && cmd[curr + 1] == '2')
+            {
+                curr += 2;
+                len = cmd.substr(curr, 4);
+                Lval = Hextodec(len);
+                curr += 4;
+            }
+            else
+            {
+                len = cmd.substr(curr, 2);
+                Lval = Hextodec(len);
+                curr += 2;
+            }
+            TLV *node = get_node_tlv_quad(curr, cmd, Lval, tag_four);
             node->child = parse_tlv(node->value);
-            // cout << node->tag << endl;
+            // cout << node->value << endl;
             TLV *last = head;
 
             if (head == NULL)
@@ -50,12 +185,33 @@ TLV *parse_tlv(char *cmd)
             curr += node->length;
             curr += node->length;
         }
-        else if (search("tlv_csv.csv", tag_two))
+        else if (search(tag_two))
         {
-            curr += 4;
-            TLV *node = get_node_tlv_double(curr, cmd);
+            curr += 2;
+            if (cmd[curr] == '8' && cmd[curr + 1] == '1')
+            {
+                curr += 2;
+                len = cmd.substr(curr, 2);
+                Lval = Hextodec(len);
+                curr += 2;
+            }
+            else if (cmd[curr] == '8' && cmd[curr + 1] == '2')
+            {
+                curr += 2;
+                len = cmd.substr(curr, 4);
+                Lval = Hextodec(len);
+                curr += 4;
+            }
+            else
+            {
+                len = cmd.substr(curr, 2);
+                Lval = Hextodec(len);
+                curr += 2;
+            }
+
+            TLV *node = get_node_tlv_double(curr, cmd, Lval, tag_two);
             node->child = parse_tlv(node->value);
-            // cout << node->tag << endl;
+            // cout << node->value << endl;
             TLV *last = head;
 
             if (head == NULL)
@@ -82,9 +238,9 @@ TLV *parse_tlv(char *cmd)
     return head;
 }
 
-int Hextodec(char *hexVal)
+int Hextodec(string hexVal)
 {
-    int len = strlen(hexVal);
+    int len = hexVal.length();
     int base = 1;
     int dec_val = 0;
 
@@ -106,67 +262,24 @@ int Hextodec(char *hexVal)
     return dec_val;
 }
 
-char *substring(char *string, int position, int length)
-{
-    char *pointer = new char[length + 1];
-
-    for (int c = 0; c < length; c++)
-    {
-        pointer[c] = string[position++];
-    }
-
-    pointer[length] = '\0';
-
-    return pointer;
-}
-
-TLV *get_node_tlv_double(int curr, char *cmd)
+TLV *get_node_tlv_double(int curr, string cmd, int length, string tag_two)
 {
     TLV *node = new TLV;
-    char len[3];
-    char tag[5];
-    len[0] = cmd[curr - 2];
-    len[1] = cmd[curr - 1];
-    len[2] = '\0';
-
-    node->length = Hextodec(len);
-    node->tag = new char[5];
-    node->value = new char[node->length];
-
-    node->tag[0] = cmd[curr - 4];
-    node->tag[1] = cmd[curr - 3];
-    node->tag[2] = '\0';
-
+    node->length = length;
+    node->tag = tag_two;
     node->next = NULL;
-    // cout << tag << endl;
-    int string_len = node->length;
-    node->value = substring(cmd, curr, 2 * string_len);
+    node->value = cmd.substr(curr, 2 * length);
 
     return node;
 }
 
-TLV *get_node_tlv_quad(int curr, char *cmd)
+TLV *get_node_tlv_quad(int curr, string cmd, int length, string tag_four)
 {
     TLV *node = new TLV;
-    char len[3];
-    char tag[5];
-    len[0] = cmd[curr - 2];
-    len[1] = cmd[curr - 1];
-    len[2] = '\0';
-
-    node->length = Hextodec(len);
-    node->tag = new char[5];
-    node->value = new char[node->length];
-    node->tag[0] = cmd[curr - 6];
-    node->tag[1] = cmd[curr - 5];
-    node->tag[2] = cmd[curr - 4];
-    node->tag[3] = cmd[curr - 3];
-    node->tag[4] = '\0';
-
+    node->length = length;
+    node->tag = tag_four;
     node->next = NULL;
-
-    int string_len = node->length;
-    node->value = substring(cmd, curr, 2 * string_len);
+    node->value = cmd.substr(curr, 2 * length);
 
     return node;
 }
@@ -191,84 +304,19 @@ void print_node(TLV *node, int level)
     }
     cout << endl;
 }
-void create()
+
+bool search(string tag)
 {
-    // file pointer
-    fstream fout;
-
-    // opens an existing csv file or creates a new file.
-    fout.open("tlv_csv.csv", ios::out | ios::app);
-
-    cout << "Enter the details of tag:";
-
-    int i, index;
-    string tag;
-
-    // Read the input
-    for (i = 0; i < 20; i++)
+    bool flg = false;
+    // cout << "rapdusize:" << sizeof(rapdu) / sizeof(rapdu[0]) << endl;
+    for (int i = 0; i < sizeof(rapdu) / sizeof(rapdu[0]); i++)
     {
-
-        cin >> index >> tag;
-
-        // Insert the data to file
-        fout << index << ", "
-             << tag << "\n";
-    }
-}
-
-bool search(string filename, char *tag)
-{
-    fstream fin;
-    fin.open(filename, ios::in);
-    vector<string> row;
-    string temp,
-        word, line;
-    int count = 0;
-
-    int i;
-    string comp = "";
-    for (i = 0; i < strlen(tag); i++)
-    {
-        comp = comp + tag[i];
-    }
-    // cout << "comp:" << comp << endl;
-
-    while (fin >> temp)
-    {
-        // cout << temp << endl;
-        row.clear();
-
-        getline(fin, line);
-        int j = 0, ind = 0;
-        stringstream lineStream(temp);
-
-        while (getline(lineStream, word, ','))
+        if (tag.compare(rapdu[i]) == 0)
         {
-            row.push_back(word);
-        }
-
-        if (row[1].compare(comp) == 0)
-        {
-            // Print the found data
-            count = 1;
-            cout << "index " << row[0] << " : \n";
-            cout << "Tag: " << row[1] << "\n";
+            flg = true;
+            // cout << "Tag: " << rapdu[i] << "\n";
             break;
         }
     }
-    if (count == 1)
-    {
-        return true;
-    }
-    return false;
-}
-string convertToString(char *a, int size)
-{
-    int i;
-    string s = "";
-    for (i = 0; i < size; i++)
-    {
-        s = s + a[i];
-    }
-    return s;
+    return flg;
 }
